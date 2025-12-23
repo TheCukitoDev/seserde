137d3292986499b0acc35a3318c9725ef98d2f2c
TheCukitoDev <145783815+TheCukitoDev@users.noreply.github.com>
2025-12-23 18:11:28 +0100

    feat (repo): Add an example for a better comprehensiom of the extractor

---
+++ examples/extractor/src/main.rs
@@ -0,0 +1,31 @@
+use eserde::Deserialize;
+use eserde_axum::Json;
+use tokio::net::TcpListener;
+
+#[tokio::main]
+async fn main() {
+    let router = axum::Router::new()
+        .route("/", axum::routing::get(|| async { "Hello, World!" }))
+        .route("/user", axum::routing::post(create_user));
+
+    let listener = TcpListener::bind("127.0.0.1:3000").await.unwrap();
+
+    axum::serve(listener, router).await.unwrap();
+}
+
+#[derive(Deserialize)]
+pub struct Contact {
+    pub email: String,
+    pub phone: String,
+}
+
+#[derive(Deserialize)]
+pub struct User {
+    pub name: String,
+    pub age: u8,
+    pub contact: Contact,
+}
+
+async fn create_user(Json(payload): Json<User>) {
+    println!("User: {}", payload.name);
+}
